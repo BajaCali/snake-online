c1267b5c5e3942e5a0c3f453eef4736b31c965b4
BajaCali <michaelnemec47@gmail.com>
2018-03-23 19:21:58 +0100

    Clean up.

--- parse.py
+++ parse.py
@@ -10,11 +10,4 @@ def Parse(text):
         print("Wrong dict data input.")
     return dic
 
-print(to_int("{dir: 555}", "dir"))
-
-dic ={}
-text = "{'dir': 12, 'height': 6, 'map': \'blablabal\', 'a': {'name': \'Jenda\'}}"
-print(Parse(text))
-print(parse(dic, text, integers, strings, snakes))
-
 
