e5f8e98f05055ecd8cafed36165381941c0d2a15
tojsemja <34749088+tojsemja@users.noreply.github.com>
2018-04-05 17:58:45 +0200

    Add files via upload

--- komunikace.py
+++ komunikace.py
@@ -1,34 +1,23 @@
 #! python 3
 
 def slucovani(slovnik):
-    
-    return str(slovnik),len(str(slovnik))
 
-def deleni(text):
-    slovnik={}
-    a=''
-    b=''
-    move=0
-    ##print("\n\n")   
-    for i in range(len(text)):
-        ##print(text[i],end="")
-        if(move!=1):
-            if text[i]==':':
-                b=a
-                a=""
-            elif text[i]==',':
-                slovnik[b]=a
-                a=''
-                b=''
-            elif text[i]=='{' and a=="":
-                a = deleni(text[i+1:-1])
-                move=1
-            elif text[i]=='}':
-                slovnik[b]=a
-                return slovnik
-            else:
-                a+=text[i]
-        elif(text[i]=="}"):
-            move=0
 
-print(deleni("{'ahoj': 'ahojky', 'kolo': 'helma', 'helma': 'kolo', 'b': 'a', 'he': {'pole': 'Brno', 'metropole': 'kurim'}, 'she': 'nothing'}"))
+
+    return str(slovnik)##,len(str(slovnik)
+
+
+def Parse(text):
+    dic = {}
+    try:
+        dic = ast.literal_eval(text)
+    except:
+        print("Wrong dict data input.")
+        return 0
+    return dic
+
+slovnik={}
+slovnik["a"]="b"
+slovnik[slovnik["a"]]="c"
+slovnik[slovnik["b"]]="abc"
+Parse(slucovani(slovnik))
